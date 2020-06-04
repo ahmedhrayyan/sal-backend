e978d3757c56d42249824dbc7bbb77abd5ecb69d
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-06-04 23:39:47 +0200

    Add AnswerSection component

---
+++ frontend/src/views/components/answerSection.tsx
@@ -0,0 +1,74 @@
+import React, { FunctionComponent, useState } from "react";
+import Avatar from "./avatar";
+import dummyAvatar from "../../images/avatar.jpg";
+import downArrow from "../../images/icons/down-arrow.svg";
+import Dropdown from "./dropdown";
+
+interface Props {}
+
+const AnswerSection: FunctionComponent<Props> = (props) => {
+  const [formActive, setFormActive] = useState<boolean>(false);
+  const [textareaVal, setTextareaVal] = useState<string>('');
+  function showForm() {
+    setFormActive(true);
+  }
+  function hideForm() {
+    setTextareaVal('')
+    setFormActive(false)
+  }
+
+  return (
+    <div className="card answer">
+      <div className="card-header">
+        <Avatar
+          src={dummyAvatar}
+          info={{ name: "Mona Kane", role: "Software Engineer" }}
+        />
+        <div className="card-header-metadata">
+          <p className="content">
+            <small>
+              1/6/2020
+              <br />
+              <span className="text-muted">Accepted by user</span>
+            </small>
+          </p>
+          <Dropdown btnContent={<img src={downArrow} alt="down-arrow icon" />}>
+            <a href="#">item 1</a>
+            <a href="#">item 2</a>
+          </Dropdown>
+        </div>
+      </div>
+      <div className="card-body">
+        <p className="card-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non
+          tellus ac justo mattis mollis. Fusce nec erat at mi tristique gravida.
+          Curabitur tempor dui ac ipsum vehicula feugiat. Fusce lacinia tellus
+          vel rhoncus commodo. Vivamus efficitur odio ac finibus interdum.
+          Praesent hendrerit libero vitae nulla sodales hendrerit.
+        </p>
+      </div>
+      <hr className="card-divider" />
+      <div className="answer-cta-section">
+        <button className="btn btn-link" onClick={showForm}>Write an answer</button>
+        <button className="btn btn-link">View all answers</button>
+      </div>
+      <hr className="card-divider" />
+      {formActive && (
+        <form action="" className="answer-form">
+          <textarea
+            name=""
+            id=""
+            rows={3}
+            className="form-control form-group"
+            value={textareaVal}
+            onChange={evt => {setTextareaVal(evt.currentTarget.value)}}
+          ></textarea>
+          <button className="btn btn-primary">Submit</button>
+          <button className="btn btn-secondary" onClick={hideForm}>Cancel</button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default AnswerSection;
--- frontend/src/views/components/index.ts
+++ frontend/src/views/components/index.ts
@@ -2,3 +2,4 @@ export { default as Navbar } from "./navbar";
 export { default as Dropdown } from "./dropdown";
 export { default as Avatar } from "./avatar";
 export { default as AskSection } from "./askSection";
+export { default as AnswerSection } from "./answerSection";
