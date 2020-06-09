aa387880eea059df4e1bf4af784db997d900b197
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-06-09 22:47:47 +0200

    Init auth0Client on loading the app

--- frontend/src/views/layouts/app.tsx
+++ frontend/src/views/layouts/app.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import config from "../../auth_config.json";
+import history from "../../state/ducks/auth0/utils";
+import { initAuth0 } from "../../state/ducks/auth0/actions";
+import { connect } from "react-redux";
 
-function App() {
+interface Props {
+  initAuth0: any
+}
+function App(props: Props) {
+  useEffect(() => {
+    const handleAuth0Redirect = (appState: any) => {
+      history.push(
+        appState && appState.targetUrl
+          ? appState.targetUrl
+          : window.location.pathname
+      );
+    };
+
+    props.initAuth0(
+      {
+        domain: config.domain,
+        client_id: config.clientId,
+        redirect_uri: window.location.origin,
+      },
+      handleAuth0Redirect
+    );
+  }, []);
+
   return <div className="App"></div>;
 }
 
-export default App;
+const mapStateToProps = {
+  initAuth0
+}
+
+export default connect(null, mapStateToProps)(App);
