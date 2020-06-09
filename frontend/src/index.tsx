53eaffd8de267fc6fe58ccac39a58949aee9e5eb
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-06-09 19:49:26 +0200

    Move App.tsx to layouts folder

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style/main.scss";
-import App from "./App";
+import { App } from "./views/layouts";
 import * as serviceWorker from "./serviceWorker";
 import { Provider as ReduxProvider } from "react-redux";
 import store from "./state/store";
