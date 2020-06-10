b285c668e731d5b06516a4285ebe06650dd0b292
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-06-10 18:29:53 +0200

    Clean up

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -6,7 +6,6 @@ def get_paginated_items(req, items, items_per_page=10):
     page = req.args.get('page', 1, int)
     start_index = (page - 1) * items_per_page
     end_index = start_index + items_per_page
-
     return items[start_index:end_index]
 
 def get_formated_questions(questions):
@@ -17,7 +16,6 @@ def get_formated_questions(questions):
             'no_of_answers': len(question.answers),
         })
         formated_questions.append(formated)
-
     return formated_questions
 
 
@@ -34,10 +32,8 @@ def create_app(test_config=None):
         all_questions = Question.query.order_by(Question.created_at).all()
         questions = get_paginated_items(request, all_questions)
         formated_questions = get_formated_questions(questions)
-
         if len(questions) == 0:
             abort(404)
-
         return jsonify({
             'success': True,
             'questions': formated_questions,
@@ -47,43 +43,34 @@ def create_app(test_config=None):
     @app.route('/questions/<question_id>', methods=['GET'])
     def get_question(question_id):
         question = Question.query.get(question_id)
-
         if question == None:
             abort(404)
-
         return jsonify({
             'success': True,
             'question': question.format()
         })
-
 
     @app.route('/questions/<question_id>', methods=['PATCH'])
     def select_best_answer(question_id):
         data = request.get_json()
         if 'answer' not in data:
             abort(400, 'answer expected in request body')
-
         answer_id = data.get('answer')
-
         question = Question.query.get(question_id)
         if question == None:
             abort(404, 'question not found')
-
         answer = Answer.query.get(answer_id)
         if answer not in question.answers:
             abort(400, 'the provided answer is not valid')
-
         question.best_answer_id = answer_id
         try:
             question.update()
         except Exception:
             abort(422)
-
         return jsonify({
             'success': True,
-            'best_answer_id': answer_id
+            'best_answer_id': int(answer_id)
         })
-
 
     @app.route('/questions', methods=['POST'])
     def post_question():
@@ -92,86 +79,67 @@ def create_app(test_config=None):
             abort(400, 'user_id expected in request body')
         if 'content' not in data:
             abort(400, 'content expected in request body')
-
         new_question = Question(data['user_id'], data['content'])
-
         try:
             new_question.insert()
         except Exception:
             abort(422)
-
         return jsonify({
             'success': True,
             'created': new_question.format()
         })
-
 
     @app.route('/questions/<question_id>', methods=['DELETE'])
     def delete_question(question_id):
         question = Question.query.get(question_id)
         if question == None:
             abort(404)
-
         try:
             question.delete()
         except Exception:
             abort(422)
-
         return jsonify({
             'success': True,
             'del_id': int(question_id)
         })
 
-
     @app.route('/questions/<question_id>/answers', methods=['GET'])
     def get_answers(question_id):
         question = Question.query.get(question_id)
-
         if question == None:
             abort(404)
-
         all_answers = question.answers
         answers = get_paginated_items(request, all_answers)
-
         if len(answers) == 0:
             abort(404)
-
         return jsonify({
             'success': True,
             'answers': [answer.format() for answer in answers],
             'no_of_answers': len(all_answers)
         })
 
-
     @app.route('/questions/<question_id>/answers/latest', methods=['GET'])
     def get_latest_answer(question_id):
         question = Question.query.get(question_id)
-
         if question == None:
             abort(404)
-
         answers = question.answers
-
         if len(answers) == 0:
-            abort(404, 'No answers for this question')
-
+            abort(404, 'no answers for this question')
         return jsonify({
             'success': True,
             'answer': answers[0].format()
         })
-
 
     @app.route('/answers/<answer_id>', methods=['GET'])
     def get_answer(answer_id):
         answer = Answer.query.get(answer_id)
         if answer == None:
             abort(404)
-
         return jsonify({
             'success': True,
             'answer': answer.format()
         })
-
 
     @app.route('/questions/<question_id>/answers', methods=['POST'])
     def post_answer(question_id):
@@ -180,38 +148,31 @@ def create_app(test_config=None):
             abort(400, 'user_id expected in request body')
         if 'content' not in data:
             abort(400, 'content expected in request body')
-
         question = Question.query.get(question_id)
         if question == None:
             abort(404, 'question not found')
-
         new_answer = Answer(data['user_id'], data['content'], question_id)
-
         try:
             new_answer.insert()
         except Exception:
             abort(422)
-
         return jsonify({
             'success': True,
             'created': new_answer.format()
         })
-
 
     @app.route('/answers/<answer_id>', methods=['DELETE'])
     def delete_answer(answer_id):
         answer = Answer.query.get(answer_id)
         if answer == None:
             abort(404)
-
         try:
             answer.delete()
         except Exception:
             abort(422)
-
         return jsonify({
             'success': True,
-            'del_id': answer_id
+            'del_id': int(answer_id)
         })
 
     # handling errors
