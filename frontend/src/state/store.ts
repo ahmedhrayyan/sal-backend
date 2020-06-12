6df40e85952d156d1bacf39ed81b523c859f20d3
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-06-12 17:54:25 +0200

    Implement apiServer redux middleware

--- frontend/src/state/store.ts
+++ frontend/src/state/store.ts
@@ -2,13 +2,17 @@ import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
 import { createStore, applyMiddleware } from "redux";
 import { auth0 } from "./ducks";
+import apiService from "./middlewares/apiService";
 
 const loggerMiddleware = createLogger();
 
 const store = createStore(
   auth0,
-  applyMiddleware(thunkMiddleware, loggerMiddleware)
+  applyMiddleware(
+    apiService,
+    thunkMiddleware,
+    loggerMiddleware
+  )
 );
 
-export type Store = typeof store;
 export default store;
