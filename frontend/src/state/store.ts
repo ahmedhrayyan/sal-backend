8015381117c2affd295b4ffd28fc797785472882
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-06-12 17:54:49 +0200

    Add questoins reducer

--- frontend/src/state/store.ts
+++ frontend/src/state/store.ts
@@ -1,13 +1,13 @@
 import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
-import { createStore, applyMiddleware } from "redux";
-import { auth0 } from "./ducks";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import * as reducers from "./ducks";
 import apiService from "./middlewares/apiService";
 
 const loggerMiddleware = createLogger();
 
 const store = createStore(
-  auth0,
+  combineReducers(reducers),
   applyMiddleware(
     apiService,
     thunkMiddleware,
