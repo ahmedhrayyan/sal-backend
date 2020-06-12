eac5335c86c6b350ab0e28a248d3459a7aaa6ff0
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-06-12 22:47:42 +0200

    Prepare AskSection component for real data

--- frontend/src/views/components/askSection.tsx
+++ frontend/src/views/components/askSection.tsx
@@ -1,14 +1,19 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, CSSProperties } from "react";
 import Avatar from "./avatar";
 import dummyAvatar from "../../images/avatar.jpg";
 import downArrow from "../../images/icons/down-arrow.svg";
 import Dropdown from "./dropdown";
 
-interface Props {}
+interface Props {
+  content: string;
+  noOfAnswers: number;
+  questionDate: Date;
+  style?: CSSProperties
+}
 
 const AskSection: FunctionComponent<Props> = (props) => {
   return (
-    <div className="card ask">
+    <div className="card ask" style={props.style}>
       <div className="card-header">
         <Avatar
           src={dummyAvatar}
@@ -17,22 +22,30 @@ const AskSection: FunctionComponent<Props> = (props) => {
         <div className="card-header-metadata">
           <p className="content">
             <small>
-              1/6/2020
+              {props.questionDate.toLocaleDateString()}
               <br />
-              <span className="text-muted">5 answers</span>
+              <span className="text-muted">
+                {props.noOfAnswers === 0
+                  ? 'No answers yet'
+                  : props.noOfAnswers === 1
+                  ? '1 answer'
+                  : `${props.noOfAnswers} answers`
+                }
+              </span>
             </small>
           </p>
-          <Dropdown btnContent={<img className="icon" src={downArrow} alt="down-arrow icon" />}>
+          <Dropdown
+            btnContent={
+              <img className="icon" src={downArrow} alt="down-arrow icon" />
+            }
+          >
             <a href="#">item 1</a>
             <a href="#">item 2</a>
           </Dropdown>
         </div>
       </div>
       <div className="card-body">
-        <p className="card-text">
-          Why are there so many YouTube channels that exploit people who are
-          desperate to work for tech companies like Google?
-        </p>
+        <p className="card-text">{props.content}</p>
       </div>
     </div>
   );
