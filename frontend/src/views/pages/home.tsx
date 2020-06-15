e65dc013e1e0dd532dd416499a3eec34609678c4
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-06-16 00:08:11 +0200

    Add basic homepage content

--- frontend/src/views/pages/home.tsx
+++ frontend/src/views/pages/home.tsx
@@ -1,9 +1,98 @@
-import React from 'react';
+import React, {
+  useEffect,
+  ReactNode,
+  Fragment,
+  useState,
+  useRef,
+  FormEvent,
+} from "react";
+import { connect } from "react-redux";
+import { loadQuestions } from "../../state/ducks/questions/actions";
+import { Question } from "../../state/ducks/questions/types";
+import { loadUser } from "../../state/ducks/users/actions";
+import { User } from "../../state/ducks/users/types";
+import { loadAnswer } from "../../state/ducks/answers/actions";
+import { AskSection } from "../components";
+import { AnswerSection } from "../components";
+import { Spinner } from "../components";
+import { QuestionForm } from "../components";
+import { Answer } from "../../state/ducks/answers/types";
 
-function Home() {
-  return (
-    <h1>Hello World!</h1>
-  )
+interface Props {
+  token: string;
+  loadQuestions: any;
+  questions: Map<number, Question>;
+  fetchingQuestions: boolean;
+  loadUser: any;
+  users: Map<number, User>;
+  loadAnswer: any;
+  answers: Map<number, Answer>;
+  fetchingUser: boolean;
 }
 
-export default Home
+function Home(props: Props) {
+  useEffect(() => {
+    // if there is not question
+    if (!props.questions.size) {
+      props.loadQuestions(props.token);
+    }
+  }, []);
+  useEffect(() => {
+    for (const question of props.questions.values()) {
+      // fetch answers
+      props.loadAnswer(
+        props.token,
+        question.best_answer || question.latest_answer
+      );
+    }
+  }, [props.questions]);
+
+  let QAComponents: ReactNode[] = [];
+  for (const question of props.questions.values()) {
+    QAComponents.push(
+      <div key={question.id}>
+        <AskSection
+          key={question.id}
+          style={{ margin: "60px 7px 0" }}
+          question={question}
+        />
+        <AnswerSection
+          bestAnswer={question.best_answer}
+          answerExists={question.no_of_answers > 0}
+          answer={props.answers.get(
+            question.best_answer || question.latest_answer
+          )}
+          questionId={question.id}
+          questionUserId={question.user_id}
+        />
+      </div>
+    );
+  }
+  return (
+    <div className="content-container">
+      <QuestionForm />
+      {props.fetchingQuestions && props.questions.size === 0 && (
+        <div className="spinner-container" style={{ height: "200px" }}>
+          <Spinner className="spinner spinner-centered" />
+        </div>
+      )}
+      {QAComponents}
+    </div>
+  );
+}
+
+function mapStateToProps(state: any) {
+  return {
+    token: state.auth0.accessToken,
+    questions: state.questions.entities,
+    fetchingQuestions: state.questions.isFetching,
+    users: state.users.entities,
+    answers: state.answers.entities,
+  };
+}
+const mapDispatchToProps = {
+  loadQuestions,
+  loadUser,
+  loadAnswer,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
