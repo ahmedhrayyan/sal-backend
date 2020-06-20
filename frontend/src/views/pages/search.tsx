05cbaadd73d66b9e949965862f4e2a9ef21d7ca1
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-06-20 09:26:53 +0200

    Add search page logic

--- frontend/src/views/pages/search.tsx
+++ frontend/src/views/pages/search.tsx
@@ -1,18 +1,84 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { ReactNode, useEffect } from "react";
+import { connect } from "react-redux";
+import { useLocation, Link } from "react-router-dom";
+import { Spinner, QuestionSection, AnswerSection } from "../components";
+import { loadSearch } from "../../state/ducks/search/actions";
 
-interface Props {}
+interface Props {
+  questions: Map<number, any>;
+  isFetchingQuestions: boolean;
+  nextPageUrl: string;
+  loadSearch: any;
+}
 function SearchPage(props: Props) {
   const location = useLocation();
   const params = new URLSearchParams(location.search);
+  useEffect(() => {
+    if (params.get('term') && !props.questions.size) {
+      props.loadSearch(params.get('term'))
+    }
+  }, [params])
 
-  if (!location.search) {
+  if (!params.get('term')) {
     return (
-      <h1>You've searched for nothing!</h1>
-    )
+      <div className="content-container">
+        <h1>You've searched for nothing!</h1>
+      </div>
+    );
   }
 
-  return <h1>You've searched for {params.get("question")}</h1>;
+  function handleFetchNewQuestions() {
+    props.loadSearch(params.get('term'))
+  }
+
+  let QComponents: ReactNode[] = [];
+  for (const question of props.questions.values()) {
+    QComponents.push(
+      <div key={question.id}>
+        <QuestionSection
+          key={question.id}
+          style={{ margin: "50px 7px 0" }}
+          question={question}
+        />
+        <div className="card answer">
+          <div className="answer-cta-section">
+            <Link to={`/questions/${question.id}`} className="btn btn-link">View Question</Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  // search top spinner condition
+  const condition = props.isFetchingQuestions && props.questions.size === 0;
+  return (
+    <div className="content-container" style={{ marginBottom: "50px" }}>
+      {/* show spinner on search page */}
+      {condition && (
+        <div className="spinner-container" style={{ height: "180px" }}>
+          <Spinner className="spinner-sm spinner-centered" />
+        </div>
+      )}
+      {QComponents}
+      {props.nextPageUrl && (
+        <div
+          style={{ textAlign: "center", marginTop: "30px" }}
+          onClick={handleFetchNewQuestions}
+        >
+          <button className="btn btn-link">Load More</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default SearchPage;
+function mapStateToProps(state: any) {
+  return {
+    questions: state.search.entities,
+    isFetchingQuestions: state.search.isFetching,
+    nextPageUrl: state.search.nextPageUrl,
+  };
+}
+const mapDispatchToProps = {
+  loadSearch
+};
+export default connect(mapStateToProps, mapDispatchToProps)(SearchPage);
