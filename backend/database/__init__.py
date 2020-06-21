c336917773d7d72fcbd8b86ece37c1371c54bcc9
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-06-21 02:07:31 +0200

    Get database_url from os.environ

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -12,9 +12,16 @@ def setup_db(app, database_uri=None, test_env=False):
     setup_db(app)
         binds a flask application and a SQLAlchemy service
     '''
-    # I have put database uri in the config file for privacy reasons
+
     if database_uri:
         app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+    else:
+        try:
+            DATABASE_URL = os.environ['DATABASE_URL']
+            app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+        except:
+            raise ValueError('DATABASE_URL expected in environ')
+
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
