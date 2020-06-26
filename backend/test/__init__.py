ae85703669649272e1323aab181f8a7848db1dac
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-06-26 13:40:49 +0200

    Update test module to work with the new api

--- backend/test/__init__.py
+++ backend/test/__init__.py
@@ -24,7 +24,7 @@ class SalTestCase(unittest.TestCase):
         db.session.commit()
 
     def test_404_get_questions(self):
-        res = self.client().get('/questions?page=1000')
+        res = self.client().get('/api/questions?page=1000')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 404)
         self.assertFalse(data['success'])
@@ -32,14 +32,14 @@ class SalTestCase(unittest.TestCase):
 
     def test_get_questions(self):
         no_of_questions = len(Question.query.all())
-        res = self.client().get('/questions')
+        res = self.client().get('/api/questions')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['success'])
         self.assertEqual(data['no_of_questions'], no_of_questions)
 
     def test_404_get_question(self):
-        res = self.client().get('/questions/232482')
+        res = self.client().get('/api/questions/232482')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 404)
         self.assertFalse(data['success'])
@@ -47,14 +47,14 @@ class SalTestCase(unittest.TestCase):
 
     def test_get_question(self):
         question_id = self.question.id
-        res = self.client().get('/questions/' + str(question_id))
+        res = self.client().get('/api/questions/' + str(question_id))
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['success'])
         self.assertTrue(data['question']['id'], question_id)
 
     def test_400_post_question(self):
-        res = self.client().post('/questions',
+        res = self.client().post('/api/questions',
                                  data={},
                                  content_type='application/json')
         data = json.loads(res.data)
@@ -63,7 +63,7 @@ class SalTestCase(unittest.TestCase):
         self.assertTrue(data['message'])
 
     def test_post_question(self):
-        res = self.client().post('/questions',
+        res = self.client().post('/api/questions',
                                  data=json.dumps({
                                      'user_id': 'test',
                                      'content': 'Is this great or what'
@@ -76,7 +76,7 @@ class SalTestCase(unittest.TestCase):
 
     def test_400_patch_question(self):
         question_id = self.question.id
-        res = self.client().patch('/questions/' + str(question_id),
+        res = self.client().patch('/api/questions/' + str(question_id),
                                   content_type='application/json')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 400)
@@ -86,7 +86,7 @@ class SalTestCase(unittest.TestCase):
     def test_patch_question(self):
         question_id = self.question.id
         answer_id = self.answer.id
-        res = self.client().patch('/questions/' + str(question_id),
+        res = self.client().patch('/api/questions/' + str(question_id),
                                   data=json.dumps({
                                       'answer': answer_id
                                   }),
@@ -94,10 +94,10 @@ class SalTestCase(unittest.TestCase):
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['success'])
-        self.assertEqual(data['best_answer_id'], answer_id)
+        self.assertEqual(data['patched']['best_answer'], answer_id)
 
     def test_404_delete_question(self):
-        res = self.client().delete('/questions/10000')
+        res = self.client().delete('/api/questions/10000')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 404)
         self.assertFalse(data['success'])
@@ -105,7 +105,7 @@ class SalTestCase(unittest.TestCase):
 
     def test_delete_question(self):
         question_id = self.question.id
-        res = self.client().delete('/questions/' + str(question_id))
+        res = self.client().delete('/api/questions/' + str(question_id))
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['success'])
@@ -113,7 +113,7 @@ class SalTestCase(unittest.TestCase):
 
     def test_404_get_answers(self):
         question_id = self.question.id
-        res = self.client().get('/questions/'+str(question_id)+'/answers?page=100')
+        res = self.client().get('/api/questions/'+str(question_id)+'/answers?page=100')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 404)
         self.assertFalse(data['success'])
@@ -122,22 +122,14 @@ class SalTestCase(unittest.TestCase):
     def test_get_answers(self):
         question_id = self.question.id
         no_of_answers = len(self.question.answers)
-        res = self.client().get('/questions/'+str(question_id)+'/answers')
+        res = self.client().get('/api/questions/'+str(question_id)+'/answers')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['success'])
         self.assertEqual(data['no_of_answers'], no_of_answers)
 
-    def test_get_latest_answer(self):
-        question_id = self.question.id
-        res = self.client().get('/questions/'+str(question_id)+'/answers/latest')
-        data = json.loads(res.data)
-        self.assertEqual(res.status_code, 200)
-        self.assertTrue(data['success'])
-        self.assertTrue(data['answer'])
-
     def test_404_get_answer(self):
-        res = self.client().get('/answers/10420')
+        res = self.client().get('/api/answers/10420')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 404)
         self.assertFalse(data['success'])
@@ -145,7 +137,7 @@ class SalTestCase(unittest.TestCase):
 
     def test_get_answer(self):
         answer_id = self.answer.id
-        res = self.client().get('/answers/'+str(answer_id))
+        res = self.client().get('/api/answers/'+str(answer_id))
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['success'])
@@ -153,7 +145,7 @@ class SalTestCase(unittest.TestCase):
 
     def test_400_post_answer(self):
         question_id = self.question.id
-        res = self.client().post('/questions/' + str(question_id) + '/answers',
+        res = self.client().post('/api/questions/' + str(question_id) + '/answers',
                                  data=json.dumps({
                                      'user_id': 'test'
                                  }),
@@ -165,7 +157,7 @@ class SalTestCase(unittest.TestCase):
 
     def test_post_answer(self):
         question_id = self.question.id
-        res = self.client().post('/questions/'+str(question_id)+'/answers',
+        res = self.client().post('/api/questions/'+str(question_id)+'/answers',
                                  data=json.dumps({
                                      'user_id': 'test',
                                      'content': 'It\'s Awesome'
@@ -177,7 +169,7 @@ class SalTestCase(unittest.TestCase):
         self.assertEqual(data['created']['question_id'], question_id)
 
     def test_404_delete_answer(self):
-        res = self.client().delete('/answers/10000')
+        res = self.client().delete('/api/answers/10000')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 404)
         self.assertFalse(data['success'])
@@ -185,7 +177,7 @@ class SalTestCase(unittest.TestCase):
 
     def test_delete_answer(self):
         answer_id = self.answer.id
-        res = self.client().delete('/answers/'+str(answer_id))
+        res = self.client().delete('/api/answers/'+str(answer_id))
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data['success'])
