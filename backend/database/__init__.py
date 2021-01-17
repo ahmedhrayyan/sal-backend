002c47d1e1c9a3fd99f14b233e6b3a2d7373aea0
Ahmed Hamed <ahmedhrayyan@outlook.com>
2021-01-17 14:43:12 +0200

    Add users table

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -17,8 +17,8 @@ def setup_db(app, database_uri=None, test_env=False):
         try:
             DATABASE_URL = os.environ['DATABASE_URL']
             app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-        except:
-            raise ValueError('DATABASE_URL expected in environ')
+        except KeyError:
+            raise KeyError('DATABASE_URL expected in environ')
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
--- backend/database/models.py
+++ backend/database/models.py
@@ -1,12 +1,37 @@
 from backend.database import db
-from sqlalchemy import Column, String, Integer,  ForeignKey, DateTime
+from sqlalchemy import Column, String, Integer,  ForeignKey, DateTime, VARCHAR, Binary, exc
 from datetime import datetime
+import bcrypt
 
 
-class Question(db.Model):
+class BaseModel:
+    def update(self):
+        try:
+            db.session.commit()
+        except exc.SQLAlchemyError as e:
+            db.session.rollback()
+            raise exc.SQLAlchemyError(e)
+
+    def delete(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+        except exc.SQLAlchemyError as e:
+            db.session.rollback()
+            raise exc.SQLAlchemyError(e)
+
+    def insert(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except exc.SQLAlchemyError as e:
+            db.session.rollback()
+            raise exc.SQLAlchemyError(e)
+
+class Question(db.Model, BaseModel):
     __tablename__ = 'questions'
     id = Column(Integer, primary_key=True)
-    user_id = Column(String(40), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     content = Column(String(), nullable=False)
     created_at = Column(DateTime(), default=datetime.utcnow, nullable=False)
     answers = db.relationship('Answer',
@@ -21,17 +46,6 @@ class Question(db.Model):
         self.user_id = user_id
         self.content = content
 
-    def update(self):
-        db.session.commit()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-
-    def insert(self):
-        db.session.add(self)
-        db.session.commit()
-
     def format(self):
         return {
             'id': self.id,
@@ -43,10 +57,10 @@ class Question(db.Model):
         }
 
 
-class Answer(db.Model):
+class Answer(db.Model, BaseModel):
     __tablename__ = 'answers'
     id = Column(Integer, primary_key=True)
-    user_id = Column(String(40), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     content = Column(String(), nullable=False)
     created_at = Column(DateTime(), default=datetime.utcnow, nullable=False)
 
@@ -57,22 +71,53 @@ class Answer(db.Model):
         self.content = content
         self.question_id = question_id
 
-    def update(self):
-        db.session.commit()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-
-    def insert(self):
-        db.session.add(self)
-        db.session.commit()
-
     def format(self):
         return {
             'id': self.id,
             'user_id': self.user_id,
             'content': self.content,
             'question_id': self.question_id,
+            'created_at': self.created_at
+        }
+
+
+class User(db.Model, BaseModel):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(VARCHAR(20), nullable=False)
+    last_name = Column(VARCHAR(20), nullable=False)
+    email = Column(VARCHAR(60), nullable=False, unique=True)
+    username = Column(VARCHAR(20), nullable=False, unique=True)
+    password = Column(Binary, nullable=False)
+    phone = Column(VARCHAR(50), nullable=True, unique=True)
+    job = Column(VARCHAR(50), nullable=True)
+    profile = Column(String, nullable=False, default="test")  # profile picture
+    created_at = Column(DateTime(), default=datetime.utcnow, nullable=False)
+
+    def __init__(self, first_name, last_name, email, username, password, job, profile=None, phone=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.username = username
+        self.password = bcrypt.hashpw(
+            bytes(password, 'utf-8'), bcrypt.gensalt(12))
+        self.phone = phone
+        self.job = job
+        if profile:
+            self.profile = profile
+
+    def checkpw(self, password):
+        return bcrypt.checkpw(bytes(password, 'utf-8'), self.password)
+
+    def format(self):
+        return {
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+            'username': self.username,
+            'phone': self.phone,
+            'job': self.job,
+            'profile': self.profile,
             'created_at': self.created_at
         }
