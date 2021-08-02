660d3c8200eee88185c2c74974460113299e2ed3
Ahmed Hamed <ahmedhrayyan@outlook.com>
2021-08-02 09:49:31 +0200

    Add answers votes logic in models

--- db/models.py
+++ db/models.py
@@ -56,6 +56,17 @@ class QuestionVote(db.Model):
     user = db.relationship('User', backref=backref(
         "questions_votes", cascade="all, delete-orphan", lazy="dynamic"))
 
+class AnswerVote(db.Model):
+    __tablename__ = "answers_votes"
+    answer_id = Column(Integer, ForeignKey('answers.id'), primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    vote = Column(Boolean, nullable=False)
+
+    answer = db.relationship('Answer', backref=backref(
+        "votes", cascade="all, delete-orphan", lazy="dynamic"))
+    user = db.relationship('User', backref=backref(
+        "answers_votes", cascade="all, delete-orphan", lazy="dynamic"))
+
 
 class User(db.Model, BaseModel):
     __tablename__ = 'users'
@@ -172,6 +183,7 @@ class Question(db.Model, BaseModel):
         ''' Check wether a specific user has voted the question '''
         return self.votes.filter_by(user=user).first() is not None
 
+
     def format(self):
         # query showing list of question's answers
         query = Answer.query.filter_by(question_id=self.id)
@@ -211,13 +223,50 @@ class Answer(db.Model, BaseModel):
         self.content = content
         self.question_id = question_id
 
+    def vote(self, user: User, vote: bool):
+        ''' upvote or downvote a question '''
+        if self.hasvoted(user):
+            # update the vote itself if the user has already voted
+            vote_obj = self.votes.filter_by(user=user).first()
+            vote_obj.vote = vote
+        else:
+            # working with the association pattern as detailed in the docs
+            # ref: https://docs.sqlalchemy.org/en/14/orm/basic_relationships.html
+            vote = AnswerVote(vote=vote)
+            vote.user = user
+            self.votes.append(vote)
+        # update in either cases
+        self.update()
+
+    def unvote(self, user: User):
+        ''' remove specific user vote '''
+        self.votes.filter_by(user=user).delete()
+        self.update()
+
+    def get_user_vote(self, user: User):
+        ''' Returns user vote for the question. None if the user has not voted'''
+        if self.hasvoted(user):
+            return self.votes.filter_by(user=user).first().vote
+        else:
+            return None
+
+    def hasvoted(self, user: User) -> bool:
+        ''' Check wether a specific user has voted the question '''
+        return self.votes.filter_by(user=user).first() is not None
+
+
     def format(self):
+        curr_user = User.query.filter_by(username=get_jwt_sub()).first()
         return {
             'id': self.id,
             'user': self.user.format(),
             'question_id': self.question_id,
             'content': self.content,
-            'created_at': self.created_at
+            'created_at': self.created_at,
+            # viewer vote will be True if upvote, False if downvote and None if the viewer has not voted
+            'upvotes': self.votes.filter_by(vote=True).count(),
+            'downvotes': self.votes.filter_by(vote=False).count(),
+            'viewer_vote': self.get_user_vote(curr_user) if curr_user else None
         }
 
 
